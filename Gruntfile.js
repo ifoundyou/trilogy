cbb141073c10b2c5548ad168c32e3036c042af1c
Jack <triumphant@163.com>
2015-02-27 22:49:09 +0800

    concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,31 @@
 module.exports = function (grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify:{
-      options:{
-        banner:'/*! <%= pkg.file %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build:{
-        src:'js/<%=pkg.file %>.js',
-        dest:'dest/<%= pkg.file %>.min.js'
-      }
-    }
-  });
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.registerTask('default', ['uglify']);
-
   // grunt.initConfig({
-  //   pkg:grunt.file.readJSON('package.json'),
-  //   concat:{
+  //   pkg: grunt.file.readJSON('package.json'),
+  //   uglify:{
   //     options:{
-  //       separator: ';'
+  //       banner:'/*! <%= pkg.file %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
   //     },
-  //     dist:{
-  //       src:['js/zepto.js', 'js/underscore.js', 'js/backbone.js'],
-  //       dest:'dest/libs.js'
+  //     build:{
+  //       src:'js/<%=pkg.file %>.js',
+  //       dest:'dest/<%= pkg.file %>.min.js'
   //     }
   //   }
   // });
-  // grunt.loadNpmTasks('grunt-contrib-concat');
-  // grunt.registerTask('default', ['concat']);
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.registerTask('default', ['uglify']);
+
+  grunt.initConfig({
+    pkg:grunt.file.readJSON('package.json'),
+    concat:{
+      options:{
+        separator: ';'
+      },
+      dist:{
+        src:['js/zepto.js', 'js/underscore.js', 'js/backbone.js'],
+        dest:'dest/lib.js'
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.registerTask('default', ['concat']);
 }
